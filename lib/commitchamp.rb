5db5e2f96453fbb406293dac67e47e948e6b93ea
violetaria <terri.chu@outlook.com>
2015-10-15 18:10:38 -0400

    added github class with init + get_contributors function

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -2,6 +2,7 @@ require "httparty"
 require "pry"
 
 require "commitchamp/version"
+require "commitchamp/github"
 # Probably you also want to add a class for talking to github.
 
 module Commitchamp
@@ -12,8 +13,20 @@ module Commitchamp
     def run
       # Your code goes here...
     end
+
+
+
   end
 end
+
+print "Please enter your Auth Token: "
+token = gets.chomp
+print "Please enter an organization: "
+org = gets.chomp
+
+## TODO do we want to check the organizations for the repos they have and have the user pick from that list?
+print "Please choose a repository: "
+repo = gets.chomp
 
 app = Commitchamp::App.new
 app.run
---
+++ lib/commitchamp/github.rb
@@ -0,0 +1,22 @@
+module Commitchamp
+  class GitHub
+    include HTTParty
+    base_uri "https://api.github.com"
+
+    def initialize(auth_token)
+      @auth = {
+          "Authorization" =>  "token #{auth_token}",
+          "User-Agent"    =>  "HTTParty"
+      }
+    end
+
+    def get_contributors(org,repo)
+      self.class.get("/repos/#{org}/#{repo}/stats/contributors",
+                    headers: @auth)
+    end
+
+  end
+end
+
+
+binding.pry
