66c5a7692784385a68952351354c9e575cc2bab3
Brit Butler <redline6561@gmail.com>
2015-10-15 14:28:57 -0400

    Add basic skeleton.

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -1,5 +1,19 @@
+require "httparty"
+require "pry"
+
 require "commitchamp/version"
+# Probably you also want to add a class for talking to github.
 
 module Commitchamp
-  # Your code goes here...
+  class App
+    def initialize
+    end
+
+    def run
+      # Your code goes here...
+    end
+  end
 end
+
+app = Commitchamp::App.new
+app.run
