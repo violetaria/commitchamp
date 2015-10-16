c750b97faa19ae9bf5be29cd228f7149684d9fca
violetaria <terri.chu@outlook.com>
2015-10-16 15:41:15 -0400

    fixed up total so it's adding adds, deletes, and changes up for each week for each user

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -18,37 +18,24 @@ module Commitchamp
       @org = prompt_user("Enter an organization: ",/^.+$/)
       @repo = prompt_user("Enter a repository: ",/^.+$/)
       ## TODO do we want to check the organizations for the repos they have and have the user pick from that list?
-      git_api = GitHub.new(@token)
-      contributors = git_api.get_contributors(@org,@repo)
-
+      contributors = query_data
       data = build_data(contributors)
-
-      sort_key = prompt_user(
-          "\nChoose a sort order:
-            1) lines added
-            2) lines deleted
-            3) total lines changed
-            4) commits made\n",/^[1234]$/).to_i
-
-      case sort_key
-        when 1
-          sort_order = :additions
-        when 2
-          sort_order = :deletes
-        when 3
-          sort_order = :total_commits
-        when 4
-          sort_order = :changes
-        else
-          sort_order = :total_commits
-          # we really should never get here due to the regex above
+      input = nil
+      until input == "Q"
+        if input == "F"
+          @repo = prompt_user("Enter a repository: ",/^.+$/)
+          contributors = query_data
+          data = build_data(contributors)
+        end
+        sort_order = get_sort_order
+        ordered_data = sort_data(data,sort_order)
+        show_data(ordered_data,sort_order)
+        input = prompt_user(
+           "Choose an option:
+            (S) Sort the data differently
+            (F) Fetch another repo
+            (Q) Quit\n",/^[FQS]$/i).upcase
       end
-
-      ordered_data = data.sort_by { |key, value| value[sort_order] }.reverse
-
-      show_data(ordered_data,sort_order)
-
-     # prompt_user("Enter (s) to Sort the data differently, (f) to Fetch another repo, or (q) to quit.",)
 
       binding.pry
     end
@@ -56,15 +43,47 @@ module Commitchamp
 
     private
 
+    def get_sort_order
+      sort_key = prompt_user(
+          "\nChoose a sort order:
+            (A) lines Added
+            (D) lines Deleted
+            (C) lines Changed
+            (T) Total Add/Delete/Changes\n",/^[ADCT]$/i).upcase
+
+      case sort_key
+        when "A"
+          :adds
+        when "D"
+          :deletes
+        when "T"
+          :total
+        when "C"
+          :changes
+        else
+          :total
+          # we should never get here but RubyMine was complaining
+      end
+    end
+
+    def sort_data(data,sort_order)
+      data.sort_by { |key, value| value[sort_order] }.reverse
+    end
+
     def show_data(data,order)
       puts
       puts "### Contributions for '#{@org}/#{@repo}'"
       puts "##  Ordered by #{order.to_s}"
       puts
-      printf("%-20s%10s%10s%10s\n", "Username","Additions","Deletions","Changes")
+      printf("%-20s%10s%10s%10s%10s\n", "Username","Additions","Deletions","Changes","Total")
       data.each do |key, value|
-        printf("%-20s%10s%10s%10s\n", "#{key.to_s}","#{value[:adds]}","#{value[:deletes]}","#{value[:changes]}")
+        printf("%-20s%10s%10s%10s%10s\n", "#{key.to_s}","#{value[:adds]}","#{value[:deletes]}","#{value[:changes]}","#{value[:total]}")
       end
+    end
+
+    def query_data
+      git_api = GitHub.new(@token)
+      git_api.get_contributors(@org,@repo)
     end
 
     def build_data(contributors)
@@ -72,18 +91,19 @@ module Commitchamp
       contributors.each do |x|
         user = x["author"]["login"].to_sym
         data[user] = Hash.new(0)
-        data[user][:total_commits] = x["total"]
         weeks = x["weeks"]
         weeks.each do |w|
           data[user][:adds] += w["a"]
           data[user][:deletes] += w["d"]
           data[user][:changes] += w["c"]
+          data[user][:total] += (w["a"] + w["d"] + w["c"])
         end
       end
       data
     end
 
     def prompt_user(text,regex)
+      puts
       print text
       input = STDIN.gets.chomp
       until input =~ regex
